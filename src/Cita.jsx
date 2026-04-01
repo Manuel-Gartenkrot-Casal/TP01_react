c438faabf72fa936e1e611cac5f751ef3675b007
Manuel-Gartenkrot-Casal <manucasal07@gmail.com>
2026-04-01 16:09:36 -0300

    Borro listado, pongo titulo a columna citas

--- src/Cita.jsx
+++ src/Cita.jsx
@@ -4,6 +4,7 @@ import CitaCaja from "./CitaCaja";
 const Cita = ({ citas }) => {
   return (
     <div className="lista-citas">
+      <h1 className="titulo">Administra tus Citas</h1>
       {citas.map((cita) => (
         <CitaCaja 
           key={cita.mascota} 
